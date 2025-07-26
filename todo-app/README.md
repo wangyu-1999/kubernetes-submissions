d58183df57880145a2a85e2aafd13847d51fc468
王昱 <wangyu2021@foxmail.com>
2025-07-26 17:40:46 +0300

    1.2

--- todo-app/README.md
+++ todo-app/README.md
@@ -1,1 +1,10 @@
 # todo-app
+
+## 1.2
+`docker build . -t march787/todo-app:v1`
+
+`docker push march787/todo-app:v1 `
+
+`kubectl create deployment todo-app-dep --image=march787/todo-app:v1`
+
+`kubectl logs -f todo-app-dep-84dddfd47d-8k2pc  `
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,7 +1,6 @@
 import app from './app.js';
 import config from './utils/config.js';
-import logger from './utils/logger.js';
 
 app.listen(config.PORT, () => {
-  logger.info(`Server is running on http://localhost:${config.PORT}`);
+  console.log(`Server is running on http://localhost:${config.PORT}`);
 });
